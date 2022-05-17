97c88999d390a0b1f9292ab0c9e632fe16ca60a1
Garet Hough <garetshough@gmail.com>
2022-05-17 14:15:54 -0700

    Finished creating a google authentication log in

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import  React, {useEffect} from "react";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { Button, View } from "react-native";
+import { StyleSheet, Text} from "react-native";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    // This URI includes a localhost url
+    webClientId:
+      "1038262737574-j0un3526ir5mkdo2cno1fl7o0v3jlnla.apps.googleusercontent.com",
+    // This URI includes a expo.go uri
+    expoClientId:
+      "1038262737574-iddu4aellun0nlvpd5auvie2o35p39pu.apps.googleusercontent.com",
+    // This URI includes a expo.go uri
+    androidClientId:
+      "1038262737574-iddu4aellun0nlvpd5auvie2o35p39pu.apps.googleusercontent.com",
+     // This URI includes a expo.go uri
+    iosClientId:
+      "1038262737574-iddu4aellun0nlvpd5auvie2o35p39pu.apps.googleusercontent.com",
+  });
+
+  const [loggedIn, setLoggedIn] = React.useState("");
+
+  useEffect(
+    () => {
+      if (response?.type === "success") {
+        const { authentication, type } = response;
+        setLoggedIn(type)
+      }
+    }, [response]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Button
+      disabled={!request}
+      title="Login"
+      onPress={() => {
+        promptAsync();
+      }}
+    />
+    {/* This will deisplay wether u are logged in or not */}
+    <Text>
+      {loggedIn === "success" ? "Logged In" : "Logged Out"}
+    </Text>
     </View>
   );
 }
@@ -13,8 +52,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
